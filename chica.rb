2cab66228e541dfda34a5ded06be03cf7f4c1a39
John Dillon <johndillon773@gmail.com>
2018-06-24 14:12:39 -0700

    updating

--- chica.rb
+++ chica.rb
@@ -2,16 +2,29 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "tilt/erubis"
 
+before do
+  t = Time.now
+  @day = t.day
+end
+
+def max_page
+  case @day
+  when 24 then 2
+  when 25 then 4
+  when 26 then 5
+  else 6
+  end
+end
+
 get "/" do
   redirect "/0"
 end
 
 get "/:index" do
   @current_page = params[:index].to_i
+
   case @current_page
-  when 0 then erb :home
-  when 1 then erb :pretty
-  when 2 then erb :traits
-  else        erb :wait, :layout => false
+  when 0..max_page then erb :layout
+  else erb :wait, :layout => false
   end
 end
