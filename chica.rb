c8e5f55c11e09c2015c129f35d29f92593304590
John Dillon <johndillon773@gmail.com>
2018-06-30 09:11:16 -0700

    last commit'

--- chica.rb
+++ chica.rb
@@ -3,16 +3,13 @@ require "sinatra/reloader" if development?
 require "tilt/erubis"
 
 before do
-  @day = Time.now.day
+  @day = #Time.now.day
 end
 
 def max_page
   case @day
-  when 24     then 2
-  when 25     then 4
-  when 26..30 then @day - 20
-  when  1     then 11
-  when  2     then 12
+  when 30 then @day - 19
+  when  1     then 20
   end
 end
 
@@ -24,7 +21,14 @@ get "/:index" do
   @current_page = params[:index].to_i
 
   case @current_page
-  when 0..max_page then erb :layout
-  else erb :wait, :layout => false
+  when 0..max_page
+    erb :layout
+  else 
+    case @day
+    when 30
+      erb :wait, :layout => false
+    when 1
+      erb :happy, :layout => false
+    end
   end
 end
